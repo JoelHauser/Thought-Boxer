438122f24d756b9cc27bee5a91ce1fccb43a10f9
Cory Hall <ComebackCory1@gmail.com>
2022-07-05 13:42:23 -0500

    added type defs schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  password: String
+  questions: [Question]
+}
+
+type Auth {
+  token: ID
+  user: User
+}
+
+type Question {
+  _id: ID
+  question: String
+  answerA: String
+  answerB: String
+  voteA: Int
+  voteB: Int
+}
+
+type Query {
+  questions: [Question]
+  question(_id: ID!): Question
+  me: User
+}
+
+type Mutation {
+  addUser(userName: String!, email: String!, password: String!): Auth
+  login(username: String!, password: String!): Auth
+  addQuestion(question: String!, answerA: String!, answerB: String!): Question
+  vote(voteA: Int!, voteB: Int!): Question
+}
+`
