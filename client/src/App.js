b86331597bdcfaf1252c442028c5bbbe05121f8f
JoelHauser <joel.hauser@gmail.com>
2022-07-05 19:40:57 -0400

    page works

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header'
 import UserColumn from './components/UserColumn';
 import QuestionColumn from './components/QuestionColumn';
