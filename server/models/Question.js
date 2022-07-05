aa516002f70af957af7023a3a477b607dc935bfd
Cory Hall <ComebackCory1@gmail.com>
2022-07-05 14:27:35 -0500

    completed question model

--- server/models/Question.js
+++ server/models/Question.js
@@ -2,6 +2,27 @@ const { Schema } = require('mongoose');
 
 const questionSchema = new Schema(
   {
-    
+    questionText: {
+      type: String,
+      required: true
+    },
+    answerA: {
+      type: String,
+      required: true
+    },
+    answerB: {
+      type: String,
+      required: true
+    },
+    voteA: {
+      type: Number,
+      required: true
+    },
+    voteB: {
+      type: Number,
+      required: true
+    }
   }
-)
+);
+
+module.exports = questionSchema;
