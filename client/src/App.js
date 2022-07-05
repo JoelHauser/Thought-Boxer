6ab7d105df3630dba34da3691d204547d6022d10
auscarter17 <98193003+auscarter17@users.noreply.github.com>
2022-07-05 19:49:43 -0400

    added footer and viewheight rules

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 import UserColumn from './components/UserColumn';
 import QuestionColumn from './components/QuestionColumn';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
         <div className="grid grid-cols-3">
           <div className="userColumn col-span-1">
@@ -18,6 +19,7 @@ function App() {
             <QuestionColumn />
           </div>
         </div>
+        <Footer />
     </div>
   );
 }
