796eb32b24b6a217376a7aab155d9d6780720626
JoelHauser <joel.hauser@gmail.com>
2022-07-05 20:54:16 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
