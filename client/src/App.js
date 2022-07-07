44c81768fafa7f4cb02dbebeecb57b190ec3ab65
TrevorTress <trevor.tress@outlook.com>
2022-07-07 19:25:51 -0400

    updated apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -12,53 +12,77 @@ import Register from './pages/Register';
 import SingleQuestionView from './pages/SingleQuestionView';
 import NoMatch from './pages/NoMatch';
 import Questions from './pages/Questions';
+import { ApolloProvider, InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-          <div className="grid grid-cols-7">
-            <div className="userColumn col-span-1">
-              <UserColumn />
-            </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="app">
+          <Header />
+            <div className="grid grid-cols-7">
+              <div className="userColumn col-span-1">
+                <UserColumn />
+              </div>
 
-            <div className="questionColumn col-span-6">
-              <Routes>
-                <Route
-                  path="/"
-                  element={<Home />}
-                />
-                <Route
-                  path="/profile"
-                  element={<Profile />}
-                />
-                <Route
-                  path="/login"
-                  element={<Login />}
-                />
-                <Route 
-                  path="/register"
-                  element={<Register />}
-                />
-                <Route 
-                  path="/questions"
-                  element={<Questions />}
-                />
-                <Route
-                  path="/question/:id"
-                  element={<SingleQuestionView />}
-                />
-                <Route
-                  path="*"
-                  element={<NoMatch />}
-                />
-              </Routes>
+              <div className="questionColumn col-span-6">
+                <Routes>
+                  <Route
+                    path="/"
+                    element={<Home />}
+                  />
+                  <Route
+                    path="/profile"
+                    element={<Profile />}
+                  />
+                  <Route
+                    path="/login"
+                    element={<Login />}
+                  />
+                  <Route 
+                    path="/register"
+                    element={<Register />}
+                  />
+                  <Route 
+                    path="/questions"
+                    element={<Questions />}
+                  />
+                  <Route
+                    path="/question/:id"
+                    element={<SingleQuestionView />}
+                  />
+                  <Route
+                    path="*"
+                    element={<NoMatch />}
+                  />
+                </Routes>
+              </div>
             </div>
-          </div>
-          <Footer />
-      </div>
-    </Router>
+            <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
