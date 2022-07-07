ff1896f02099edd515feb727bb30d36bdb706125
TrevorTress <trevor.tress@outlook.com>
2022-07-06 23:19:40 -0400

    fixed app bug, added register page framework

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+// import { ADD_USER } from '../utils/mutations';
+// import Auth from '../utils/auth';
 
 function Register() {
+    // const [addUser, { error }] = useMutation(ADD_USER);
+    // const [formState, setFormState] = useState({
+    //     username: '',
+    //     email: '',
+    //     password: '',
+    // });
+    
     return (
-        <div>Sign up for Thought Boxer.</div>
+        <main>
+            <h1 className='text-center font-bold'>
+                Sign up for Thought Boxer
+            </h1>
+            <form /*onSubmit={handleFormSubmit}*/>
+                <input
+                    className="form-input"
+                    placeholder="Enter a Username"
+                    name="username"
+                    type="username"
+                    id="username"
+                    // value={formState.username}
+                    // onChange={handleChange}
+                />
+                <input
+                    className="form-input"
+                    placeholder="Choose a Password"
+                    name="password"
+                    type="password"
+                    id="password"
+                    // value={formState.password}
+                    // onChange={handleChange}
+                />
+                <button className="btn justify-center" type="submit">
+                    Submit
+                </button>
+            </form>
+            {/* {error && <div>Signup failed</div>} */}
+        </main>
     )
 }
 
