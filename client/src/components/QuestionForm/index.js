54ffeb6a6d015516d30db122aec5124ef02aa0a6
Cory Hall <ComebackCory1@gmail.com>
2022-07-09 22:06:42 -0500

    fixed a stylesheet bug

--- client/src/components/QuestionForm/index.js
+++ client/src/components/QuestionForm/index.js
@@ -42,7 +42,7 @@ const QuestionForm = () => {
 
         try {
             await addQuestion({
-                variables: {...formState },
+                variables: { ...formState },
             });
 
             setFormState({
