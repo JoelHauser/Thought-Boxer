221da81e87c81c0006f2d07ed1daa2e8cb986dbd
JoelHauser <joel.hauser@gmail.com>
2022-07-10 13:19:27 -0400

    smol update

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import { ApolloProvider, InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Footer from './components/Footer';
-import UserColumn from './components/UserColumn';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import SingleQuestionView from './pages/SingleQuestionView';
-import NoMatch from './pages/NoMatch';
-import Questions from './pages/Questions';
-import QuestionForm from './components/QuestionForm';
-
+import Footer from "./components/Footer";
+import UserColumn from "./components/UserColumn";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SingleQuestionView from "./pages/SingleQuestionView";
+import NoMatch from "./pages/NoMatch";
+import Questions from "./pages/Questions";
+import QuestionForm from "./components/QuestionForm";
+import '../src/global.scss';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
 
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -50,38 +55,14 @@ function App() {
 
             <div className="questionColumn col-span-6">
               <Routes>
-                <Route
-                  path="/"
-                  element={<Home />}
-                />
-                <Route
-                  path="/profile"
-                  element={<Profile />}
-                />
-                <Route
-                  path="/login"
-                  element={<Login />}
-                />
-                <Route
-                  path="/register"
-                  element={<Register />}
-                />
-                <Route
-                  path="/questions"
-                  element={<Questions />}
-                />
-                <Route
-                  path="/question/:id"
-                  element={<SingleQuestionView />}
-                />
-                <Route
-                  path='questionform'
-                  element={<QuestionForm />}
-                />
-                <Route
-                  path="*"
-                  element={<NoMatch />}
-                />
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/questions" element={<Questions />} />
+                <Route path="/question/:id" element={<SingleQuestionView />} />
+                <Route path="questionform" element={<QuestionForm />} />
+                <Route path="*" element={<NoMatch />} />
               </Routes>
             </div>
           </div>
