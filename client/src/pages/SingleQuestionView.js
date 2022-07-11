d76d0b7183a1493ed7b45f759052c10bfb0a5c3d
auscarter17 <98193003+auscarter17@users.noreply.github.com>
2022-07-11 01:59:35 -0400

    ratio bar functioning

--- client/src/pages/SingleQuestionView.js
+++ client/src/pages/SingleQuestionView.js
@@ -35,6 +35,9 @@ const SingleQuestionView = () => {
 
     const percentageA = Math.round( (question.voteA - question.voteB) / ( (question.voteA + question.voteB) / 2 ) * 100 );
     const percentageB = (100 - percentageA)
+    const ratioWidth = (percentageA * 5);
+    // create const for ratiobar width
+    // create new formula for width
 
     if (loading) {
         return <div>Loading...</div>
@@ -53,6 +56,11 @@ const SingleQuestionView = () => {
                 >{question.answerB}
             </button>
             <p>{percentageA}% chose answer {question.answerA}. {percentageB}% chose {question.answerB}.</p>
+            <div className="barContainer">
+                <div className="bg-gray-500 ratioBar">
+                    <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none ratioBar ratioBarFull" style={{width:ratioWidth }}></div>
+                </div>
+            </div>
         </div>
     )
 }
