938e27b4fb4951c009a1b312636fd8e13c3688b0
Cory Hall <ComebackCory1@gmail.com>
2022-07-10 23:02:29 -0500

    added basic style to question form page

--- client/src/components/QuestionForm/index.js
+++ client/src/components/QuestionForm/index.js
@@ -1,34 +1,12 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_QUESTION } from '../../utils/mutations';
-import { QUERY_QUESTIONS, QUERY_ME } from '../../utils/queries';
 
 
 const QuestionForm = () => {
     const [formState, setFormState] = useState({ title: '', questionText: '', answerA: '', answerB: '' });
-    const [characterCount, setCharacterCount] = useState(0);
 
     const [addQuestion, { error }] = useMutation(ADD_QUESTION);
-
-    // const [addQuestion, { error }] = useMutation(ADD_QUESTION, {
-    //     update(cache, { data: { addQuestion } }) {
-    //         try {
-    //             const { me } = cache.readQuery({ query: QUERY_ME });
-    //             cache.writeQuery({
-    //                 query: QUERY_ME,
-    //                 data: { me: { ...me, questions: [...me.questions, addQuestion] } }
-    //             });
-    //         } catch (e) {
-    //             console.warn("First question insertion by user!")
-    //         }
-
-    //         const { questions } = cache.readQuery({ query: QUERY_QUESTIONS });
-    //         cache.writeQuery({
-    //             query: QUERY_QUESTIONS,
-    //             data: { questions: [addQuestion, ...questions] },
-    //         });
-    //     }
-    // });
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -65,8 +43,8 @@ const QuestionForm = () => {
 
 
     return (
-        <div className='flex flex-col items-center'>
-            <h1>Add a question!</h1>
+        <div className='flex flex-col items-center mt-5 divide-y-2'>
+            <h1 className='text-2xl mb-2'>Share Your Thoughts</h1>
             <form className='w-3/5' onSubmit={handleFormSubmit}>
                 <div className='form-box'>
                     <label>Describe your dilemma</label>
@@ -121,7 +99,7 @@ const QuestionForm = () => {
                         value={formState.answerB}
                     />
                 </div>
-                <button className='' type='submit'>
+                <button className='px-2 py-1 rounded' type='submit'>
                     Submit
                 </button>
             </form>
