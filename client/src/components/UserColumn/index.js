0c12583873ef09d9b87ce7dfb7e06ea9520aeab9
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-07-11 18:46:16 -0400

    text spans for bar

--- client/src/components/UserColumn/index.js
+++ client/src/components/UserColumn/index.js
@@ -18,55 +18,55 @@ const UserColumn = () => {
     return (<h1>Loading...</h1>)
   } else {
     //
-    const me = userData.me
-    const myQuestions = me.questions
-    console.log(myQuestions)
+    // const me = userData.me
+    // const myQuestions = me.questions
+    // console.log(myQuestions)
     //
 
     //
-    return (
-      <div className="text-center">
-        {Auth.loggedIn() ? (
-          <>
-            <div className="profilecontainer">
-              <h1>Hello, {me.username}</h1>
-              <p>Questions Asked: {me.questions.length}</p>
-              <p>Voted Posts: {me.votes.length}</p>
-              <p>Your Questions:</p>
-              <div>
-                {myQuestions.map( question => (
-                  <div key={question._id}>
-                    <Link
-                      to={{
-                        pathname:`/question/${question._id}`
-                      }}
-                    >
-                      <p>{question.title}</p>                          
-                    </Link>
-                    <p>Votes: {question.voteA + question.voteB}</p>  
-                  </div>
-                ))}
-            </div>
+    // return (
+    //   <div className="text-center">
+    //     {Auth.loggedIn() ? (
+    //       <>
+    //         <div className="profilecontainer">
+    //           <h1>Hello, {me.username}</h1>
+    //           <p>Questions Asked: {me.questions.length}</p>
+    //           <p>Voted Posts: {me.votes.length}</p>
+    //           <p>Your Questions:</p>
+    //           <div>
+    //             {myQuestions.map( question => (
+    //               <div key={question._id}>
+    //                 <Link
+    //                   to={{
+    //                     pathname:`/question/${question._id}`
+    //                   }}
+    //                 >
+    //                   <p>{question.title}</p>                          
+    //                 </Link>
+    //                 <p>Votes: {question.voteA + question.voteB}</p>  
+    //               </div>
+    //             ))}
+    //         </div>
   
   
   
-              <a href="/" onClick={logout}>
-                Logout
-              </a>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="login">
-              <Link to="/login">Login</Link>
-            </div>
-            <div className="regsitser">
-              <Link to="/register">Register</Link>
-            </div>
-          </>
-        )}
-      </div>
-    );
+    //           <a href="/" onClick={logout}>
+    //             Logout
+    //           </a>
+    //         </div>
+    //       </>
+    //     ) : (
+    //       <>
+    //         <div className="login">
+    //           <Link to="/login">Login</Link>
+    //         </div>
+    //         <div className="regsitser">
+    //           <Link to="/register">Register</Link>
+    //         </div>
+    //       </>
+    //     )}
+    //   </div>
+    // );
     //
   }
 }
