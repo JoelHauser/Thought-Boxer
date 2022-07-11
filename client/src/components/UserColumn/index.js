26c635d0ebda59bb5609e0e60e991a99a3ccc921
TrevorTress <trevor.tress@outlook.com>
2022-07-11 00:21:54 -0400

    user column and user type updates

--- client/src/components/UserColumn/index.js
+++ client/src/components/UserColumn/index.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
+import { useQuery } from '@apollo/client';
+import { QUERY_ME, QUERY_QUESTIONS } from '../../utils/queries';
 
 function UserColumn() {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
+  const meData = useQuery(QUERY_ME);
+  const me = meData.data.me;
+  console.log()
+
+  // const { questionData } = useQuery(QUERY_QUESTIONS);
+  // const questions = questionData?.questions || {};
+
   return (
     <div className="text-center">
       {Auth.loggedIn() ? (
         <>
           <div className="profilecontainer">
-            <Link to="/profile">My Profile</Link>
+            <h1>Hello, {me.username}</h1>
+            <p>Questions Asked: {me.questions.length}</p>
+            <p>Voted Posts: {me.votes.length}</p>
             <a href="/" onClick={logout}>
               Logout
             </a>
