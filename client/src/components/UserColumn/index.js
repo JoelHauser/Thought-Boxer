18d9ae168fff48aa9d669c6b7acae24d83723fe9
TrevorTress <trevor.tress@outlook.com>
2022-07-11 01:42:16 -0400

    user column updates

--- client/src/components/UserColumn/index.js
+++ client/src/components/UserColumn/index.js
@@ -10,38 +10,60 @@ function UserColumn() {
     Auth.logout();
   };
 
-  const meData = useQuery(QUERY_ME);
-  const me = meData.data.me;
-  console.log()
+  const { loading, data: userData } = useQuery(QUERY_ME);
 
-  // const { questionData } = useQuery(QUERY_QUESTIONS);
-  // const questions = questionData?.questions || {};
+  if(loading) {
+    return (<h1>Loading...</h1>)
+  } else {
+    const me = userData.me
+    const myQuestions = me.questions
+    console.log(myQuestions)
+    
 
-  return (
-    <div className="text-center">
-      {Auth.loggedIn() ? (
-        <>
-          <div className="profilecontainer">
-            <h1>Hello, {me.username}</h1>
-            <p>Questions Asked: {me.questions.length}</p>
-            <p>Voted Posts: {me.votes.length}</p>
-            <a href="/" onClick={logout}>
-              Logout
-            </a>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="login">
-            <Link to="/login">Login</Link>
-          </div>
-          <div className="regsitser">
-            <Link to="/register">Register</Link>
-          </div>
-        </>
-      )}
-    </div>
-  );
+    return (
+      <div className="text-center">
+        {Auth.loggedIn() ? (
+          <>
+            <div className="profilecontainer">
+              <h1>Hello, {me.username}</h1>
+              <p>Questions Asked: {me.questions.length}</p>
+              <p>Voted Posts: {me.votes.length}</p>
+              <p>Your Questions:</p>
+              <div>
+                {myQuestions.map( question => (
+                  <div key={question._id}>
+                    <Link
+                      to={{
+                        pathname:`/question/${question._id}`
+                      }}
+                    >
+                      <p>{question.title}</p>                          
+                    </Link>
+                    <p>Votes: {question.voteA + question.voteB}</p>  
+                  </div>
+                ))}
+            </div>
+  
+  
+  
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="login">
+              <Link to="/login">Login</Link>
+            </div>
+            <div className="regsitser">
+              <Link to="/register">Register</Link>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 // <Link to="/login">
